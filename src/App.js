18df6c279b3d16a297ed3e6aaa233edffb8562fd
shrav-prakash <shravanprakash2@gmail.com>
2023-06-21 13:34:50 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const count = event => {
+    let para = event.target.value.split(" ").join(',').split('.').join(',').split('?').join(',').split('!').join(',').split(',');
+    para = para.filter(word => {
+      return word != ''
+    })
+    let p = document.getElementById('count');
+    p.innerText = "Word Count: " + para.length;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Responsive Paragraph Word Counter</h1>
+      <textarea onChange={count} rows="8" cols="80" />
+      <p id="count">Word Count: 0</p>
+    </>
   );
 }
 
